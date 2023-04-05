2d736d89690879bdb0705b7c36f51c010332385e
Zohid7717 <101669736+Zohid7717@users.noreply.github.com>
2023-04-05 14:22:33 +0500

    layk tayyor emas

--- src/Components/CardItem/CardItem.jsx
+++ src/Components/CardItem/CardItem.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './CardItem.scss'
 import { Link } from 'react-router-dom';
+import { BiLike } from 'react-icons/bi';
+import { AiFillLike } from 'react-icons/ai'
 
 const CardItem = ({ item, id, page }) => {
+  const likeArr = []
+  const like = () => {
+    
+    // localStorage.setItem('item', `${id}`)
+    console.log(id)
+  }
   return (
     <div className='cardItem'>
       <img src={item.image} alt="" />
-      <p className="cardItem__status">{item.status }</p>
+      <p className="cardItem__status">{item.status}</p>
       <div className="cardItem__content">
         <h3 className="cardItem__name">
           {item.name}
@@ -17,7 +25,10 @@ const CardItem = ({ item, id, page }) => {
         <p className="cardItem__last-loc-title">
           {item.location.name}
         </p>
-        <Link to={`${id}`} className="cardItem__more">More</Link>
+        <div className="cardItem__btn-wrap">
+          <Link to={`${id}`} className="cardItem__more">More</Link>
+          <BiLike onClick={like} className='cardItem__like'/><AiFillLike/>
+        </div>
       </div>
     </div>
   );
